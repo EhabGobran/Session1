dec3497f23d103683ef4237e37cc1d0da18506ee
EhabGobran <ehapgobarn@gmail.com>
2024-06-25 01:35:11 +0300

    index1

--- index1.php
+++ index1.php
@@ -4,8 +4,50 @@
   $text ='Ehab';
 
   echo 'hi im '. $text .'. and im a well known developer in the industry ';
-  echo 'im living my golden era '. $G .'years old ';
+  echo 'im living my golden era in my '. $G .'year old ';
+  echo "<br>";
+  // case sensitive
+  $s=33;
+  $S=44;
+  echo $s."<br>";
+  echo $S."<br>";
 
+  // php assign by value as a default
+  // php is a loosely typed lang
+  $mana= 360;
+  $Q = $mana;
+  $mana=140;
+  
+  echo $Q."<br>";
+  echo $mana."<br>";
+
+    // single line / doc block/* */
+    /*
+    *
+    *
+    *
+    */
+    
+    $myName='Ehab'; $myLastName='Gobran';
+    echo $myName."<br>";
+    echo '$Myname'."<br>";
+    echo 'my name is $myName $myLastName'."<br>";
+    echo "my name is $myName $myLastName"."<br>";
+    echo "my name is {$myName} {$myLastName}"."<br>";
+    echo 'my name is {$myName} {$myLastName}'."<br>";
+
+    define("database","const");
+    echo database;
+    // database = "123";  throw an error
+
+    function printed()
+    {
+      global $first;
+      echo $first;
+    }
+    printed();
+     
+  
   
 ?>
 
