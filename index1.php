a78dbb578a0c2e62a37908ada3af02331d1a09ac
EhabGobran <ehapgobarn@gmail.com>
2024-06-25 01:32:25 +0300

    index1

--- index1.php
+++ index1.php
@@ -1,44 +1,24 @@
 <?php
-$x= array('ahmed','pop','bob');
-$y=['ehab','doList'];
-$z=array('','');
-$dob=array('big','over',33,'end',[33]);
-$xToDegree=array(3=>'ahmed',80=>'pop',93=>'bob');
 
-$degreesOfSTD = ['ahmed' => 50, 'mohamed' => 256,'sayed' => 4,'gamal' => 7];
-$degreesOfSTD[] =55;
-$classes = [
-    ['a' , 'b' , 'c'] ,
-    ['d' , 'e' , 'f'] ,
-    ['g' , 'h' , 'i'] ,
-  ];
-  $statusOfclient='user';
-  $logged_in=true;
+  $G= '33';
+  $text ='Ehab';
 
-  $status = $logged_in===true ?(  $statusOfclient==='user' ?
-                                  '<button>logout</button>':
-                                  '<button>logout</button><button>dashboard</button>'  )
-                                  :'<button>login</button>';
-
-  $s1=true;
-  $s2=true;
-  $s3=false;
-
-  var_dump((s1&&s2)||(s1||s3));
-  echo "im ehab";
-  
+  echo 'hi im '. $text .'. and im a well known developer in the industry ';
+  echo 'im living my golden era '. $G .'years old ';
 
   
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
+  <p>ehab</p>
     
 </body>
 </html>
